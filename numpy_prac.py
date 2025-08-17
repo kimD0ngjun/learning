2537409210a6af4736a8437d69e1cf91ce03de8a
kimD0ngjun <chickentasty0112@gmail.com>
2025-08-17 16:16:42 +0900

    numpy: 행렬에서의 파이썬 인덱싱, 슬라이싱, 행렬 사칙연산과 행렬곱

--- numpy_prac.py
+++ numpy_prac.py
@@ -39,3 +39,79 @@ print(eye_mat)
 
 random_mat = np.random.random((2,2)) # 임의의 값으로 채워진 배열 생성
 print(random_mat)
+
+# 0부터 9까지
+range_vec = np.arange(10)
+print(range_vec)
+
+n = 2
+range_n_step_vec = np.arange(1, 10, n) # 파이썬 슬라이싱 문법 그대로 적용
+print(range_n_step_vec)
+
+# 30까지 어레인징하고 5행 6열의 배열로 리셰이핑
+reshape_mat = np.array(np.arange(30)).reshape((5,6))
+print(reshape_mat)
+
+"""
+행렬의 슬라이싱, 인덱싱
+"""
+# numpy는 파이썬 슬라이싱 그대로 통용 가능
+# [행에 대한 슬라이싱, 열에 대한 슬라이싱]
+mat = np.array([[1, 2, 3], [4, 5, 6]])
+print(mat)
+
+# 첫번째 행 출력
+slicing_mat = mat[0, :]
+print(slicing_mat)
+
+# 두번째 행 두번째 요소부터 2칸씩
+slicing_mat = mat[1, 1::2]
+print(slicing_mat)
+
+# 두번째 열 출력
+slicing_mat = mat[:, 1]
+print(slicing_mat)
+
+# 특정 위치의 원소만을 갖고오거나 그런 원소들 간의 조합으로는
+# 인덱싱을 써야 함. [행의 인덱스, 열의 인덱스]
+mat = np.array([[1, 2], [4, 5], [7, 8]])
+print(mat)
+
+# 1행 0열의 원소
+# => 0부터 카운트하므로 두번째 행 첫번째 열의 원소.
+print(mat[1, 0])
+
+# mat[[2행, 1행],[0열, 1열]]
+# 각 행과 열의 쌍을 매칭하면 2행 0열, 1행 1열의 두 개의 원소.
+indexing_mat = mat[[2, 1],[0, 1]]
+print(indexing_mat)
+
+"""
+행렬 사칙연산
+* 벡터곱과 다르다 그냥 요소간 위치 맞춰서 사칙연산할 뿐
+* 행렬 구조가 서로 다르면 에러가 발생
+"""
+x = np.array([1,2,3])
+y = np.array([4,5,6])
+
+# result = np.add(x, y)와 동일.
+result = x + y
+print(result)
+
+# result = np.subtract(x, y)와 동일.
+result = x - y
+print(result)
+
+# result = np.multiply(result, x)와 동일.
+result = result * x
+print(result)
+
+# result = np.divide(result, x)와 동일.
+result = result / x
+print(result)
+
+# 행렬 간 곱을 하려면 np.dot(행렬1, 행렬2) 메소드를 호출한다
+mat1 = np.array([[1,2],[3,4]])
+mat2 = np.array([[5,6],[7,8]])
+mat3 = np.dot(mat1, mat2)
+print(mat3)
